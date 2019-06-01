61974da11da013bc9920b32a84db3721c65dbf88
MEGAN ADAMS <meganadams@MEGANs-MacBook-Air.local>
2019-06-01 17:49:52 -0500

    committing location

---
+++ scripts/weather.js
@@ -0,0 +1,42 @@
+const url = "http://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "fc480c5aaec93162c2d8fd255dd1bfdb";
+
+
+//event listener for activity button
+document.querySelector("#activity-btn-run").addEventListener(
+    "click",
+    function(e) {
+        //prevent default behavior (ie. page reload)
+        e.preventDefault();
+        //console.log for tracking/testing
+        console.log("activity-btn-run clicked");
+        //location variable for future purposes
+        //const location = document.querySelector("#location").value;
+
+        //document.querySelector("#location").value = "";
+
+        //fetch data from openweathermap api
+        fetch(url + "austin" + "&appid=" + apiKey) //replace Austin with location after testing complete
+            .then(function(response) {
+                console.log(response.json());
+                return response.json();
+            })
+            .then(function(response) {
+                updateUISuccess(response);
+            })
+            .catch(function() {
+                updateUIFailure();
+            });
+    },
+    false
+);
+
+//function to update the page if the api call is successful
+function updateUISuccess() {
+    console.log("updateUISuccess run");
+};
+
+//function to update the page if the api call returns an error
+function updateUIFailure() {
+    console.log("updateUIFailure run");
+};
