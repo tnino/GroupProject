cff3e42ad2a2238c3459463ad793b301dcd3a1a6
lacasey <lacasey@homeaway.com>
2019-06-01 15:21:19 -0500

    initial commit

---
+++ scripts/location.js
@@ -0,0 +1,9 @@
+//const url = 'http://api.ipstack.com/';
+//const key = '8055339b7e1902b5583fe7b534566309';
+
+//run requester ip lookup
+
+
+
+//include callback to rerun api call with requester ip
+//return location data for use within weather.js
